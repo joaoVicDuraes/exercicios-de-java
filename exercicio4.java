c535e3db5a45d64b34d01f5e6a5bd1c5d6543184
Joao Victor <joao.vod06@gmail.com>
2024-10-13 10:18:44 -0300

    exercicio 4 feito

---
+++ exercicio4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class exercicio4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int antecessor = n-1;
+        int posterior = n+1;
+
+        System.out.println(antecessor + " " +  posterior);
+    }
+}
